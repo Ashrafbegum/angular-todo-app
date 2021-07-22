ccaba8ff64aa0ef70388552c0a2d46d4db60e292
ashraf <ashrafunnisa931@gmail.com>
2021-07-23 00:23:43 +1000

    Add functions to  add and delete tasks
    Add a fucntion toggleDone to set the completion status of task

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -9,20 +9,43 @@ import { Todo } from './../../models/Todo';
 export class TodosComponent implements OnInit {
 
   todos:Todo[] = [];
+  inputTodo:string = "";
+
   // todos: Todo[] = [
   //   new Todo("XYZ", false),
   //   new Todo("Ram", true),
   //   new Todo("andrew", true)
   // ];
+
   constructor() { }
 
   ngOnInit(): void {
       this.todos = [
-      new Todo("John", false),
-      new Todo("Ram", true),
-      new Todo("andrew", true)
+      new Todo("Learn AngularJs", false),
+      new Todo("Learn Typescript", true),
+      new Todo("Learn React", true)
     ]
   }
 
-}
+  //method to mark a task as completed or not completed 
+  toggleDone(id:number){
+    this.todos.map((v,i) => {
+      console.log(v)
+       if(i == id) v.completed = !v.completed  // If a task's completed is true then set it to false and vice versa
+       return v;
+    })
+  }
 
+  deleteTodo(id:number) {
+    this.todos = this.todos.filter((v,i) => i !== id);
+  }
+
+  addTodo() {
+    this.todos.push({
+      task: this.inputTodo, 
+      completed: false
+    })
+    this.inputTodo = ""  // Set input field to empty after the task is added to the list
+  }
+
+}
